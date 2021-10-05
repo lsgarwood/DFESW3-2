ee5840946fd6ea5996df97d6a37df9bbe3289358
Lauren Garwood <ls_garwood@yahoo.co.uk>
2021-10-05 15:42:50 +0100

    new exercises

---
+++ functions.py
@@ -0,0 +1,30 @@
+#def add_calc(number1, number2):
+#    answer = number1 + number2
+#    return answer
+
+#added_number = add_calc(5,5)
+#print(added_number + 20)
+
+#def maxOf3 (num1, num2,num3):
+#    numlist= [num1,num2,num3]
+#    return f' largest value is {max(numlist)}'
+#dig1=int(input('enter a number: '))
+#dig2=int(input('enter a number: '))
+#dig3=int(input('enter a number: '))
+#print(maxOf3(dig1, dig2, dig3))
+
+#def maxNum(numList):
+#    max = numList[0]
+#    for x in numList:
+#        if x > max:
+#            max = x
+#    return max
+
+#nums3 = input('Please enter three numbers separated by spaces:')
+#numList = nums3.split()
+#for i in range(len(numList)):
+#    numList[i] = int(numList[i])
+
+#print("Largest number is: ", (maxNum(numList)))
+
+
